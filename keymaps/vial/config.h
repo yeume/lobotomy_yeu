48eb8ed426f19b18b6270fc64830d576fd091da6
yeume <lolitsjae@gmail.com>
2025-07-22 23:29:37 -0500

    init2

---
+++ keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB6, 0x2E, 0x3C, 0x44, 0xEC, 0xB0, 0x34, 0x43}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
